2e84882837567cc27af3570c9f6b8b5ac66bc616
Unknown <torres-jacobo1@hotmail.com>
2018-12-13 16:53:10 -0500

    updates

--- main_audio.py
+++ main_audio.py
@@ -1,4 +1,4 @@
-import convert_wav_with_features,noise_s,cut,audiospeed
+import convert_wav_with_features,noise_s,cut,audiospeed,silencio,duplicado_audio
 import Extraccion_Fondo
 from werkzeug.utils import secure_filename
 import os
@@ -7,30 +7,39 @@ import spte_noise
 import prue
 
 def audio(file,paths,filenames,user):
-      
 
+	#file: archivo ingresado 
+	#paths: ruta a almacenar los audios procesados
+	#filenames: nombre del archivo ingresado
+
+    #Guarda audio original en la ruta ./audio
 	file.save(os.path.join(paths,filenames))
 
-	#convert to format wav with features 
-	#"name " is the name of audio file 
+	#Convierte cualquier audio en formato .wav / name es el nombre del archivo convertido .wav 
 	name=convert_wav_with_features.audio(paths,filenames,user)
 
-	#modify audio speed to 0.5
+	#Disminuye la velocidad del audio a 0.5 de la actual
 	audiospeed.input_pathaudio(paths,name,user)
 
-	#separating audio every 30 seconds
+	#Separa el audio cada 30 segundos
 	cut.separate_audio(paths,name,user)
 
-	#noise
+	#Agrega ruido al audio
 	noise_s.path(paths,name,user)
 
-	#fondo
+	#agregando silencio de 0.65s
+	s_audio=silencio.agregar(paths,name)
+
+	#duplicando audio
+	duplicado_audio.execute(paths,s_audio)
+
+	#Extrae el fondo del audio --- proceso necesario para la limpieza del audio
 	fil=Extraccion_Fondo.Extrac_Sonido(paths,filenames,user)
 
-	#sin fondo
+	#Audio limpio, recorta vacios, disminuye ruidos 
 	spte_noise.audioclean(paths,name,fil,user)
 
-	#Espectograma
+	#Genera espectogramas
 	spg.plotstft(paths+"/"+name)
 
 	#Estadisticas
